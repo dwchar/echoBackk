38c101d7b76a0593bdde8c88815ba7e71e8f24e5
dwchar <dwchar@users.noreply.github.com>
2017-01-17 16:23:28 -0700

    updated with credit
    Used this code to help with some annoying websites. From Hacker Quarterly 2014. Thanks r00tNinjas for some help reading your output

--- trap.php
+++ trap.php
@@ -1,19 +1,21 @@
 <html>
-  <title> Oh My </title>  
-  <body> 
-  <center><p>Now what ???
-  </p></center>
-  </body>
-<?php  
-  $bl_filename = "/etc/security/blacklistip";
-  $f = fopen($bl_filename,'a');
-  $msg = $_SERVER['REMOTE_ADDR']."\tblack\n";
-  fwrite($f,$msg);
-  fclose($f);
-  
-  $bl_filename = "/etc/security/offense.log";
-  $f = fopen($bl_filename,'a');
-  $msg = $_SERVER['REQUEST_TIME']."\t".$_SERVER['REMOTE_ADDR']."\n";
-  fwrite($f,$msg);
-  fclose($f);
-  ?></html>
+	<title> Oh My </title>
+	<body>
+		<center>
+			<p>Now what ???</p>
+		</center>
+	</body>
+<?php
+	$bl_filename = "/etc/security/blacklistip";
+	$f = fopen($bl_filename,'a');
+	$msg = $_SERVER['REMOTE_ADDR']."\tblack\n";
+	fwrite($f,$msg);
+	fclose($f);
+	
+	$bl_filename = "/etc/security/offenses.log";
+	$f = fopen($bl_filename,'a');
+	$msg = $_SERVER['REQUEST_TIME']."\t".$_SERVER['REMOTE_ADDR']."\n";
+	fwrite($f,$msg);
+	fclose($f);
+	?>
+</html>
